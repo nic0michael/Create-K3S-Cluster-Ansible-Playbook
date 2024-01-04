9476c2e08a955aa7fe9a3116630fce3a7a9548e9
“nico@loyaltyplus.cloud” <nico@loyaltyplus.cloud>
2024-01-04 11:05:11 +0200

    added txt files

--- playbooks/start_other_master_node_in_k3s_cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/#!/usr/bin/env bash
-
-# to launch this playbook : start_other_master_node_in_k3s_cluster.yml
-
-ansible-playbook /opt/semaphore/Create-K3S-Cluster-Ansible-Playbook/playbooks/start_other_master_node_in_k3s_cluster.yml
