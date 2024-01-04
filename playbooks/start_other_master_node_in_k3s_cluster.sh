d37bb39973458d665ee00823c0bd4a425914237b
“nico@loyaltyplus.cloud” <nico@loyaltyplus.cloud>
2024-01-04 10:04:01 +0200

    added shell scripts

---
+++ playbooks/start_other_master_node_in_k3s_cluster.sh
@@ -0,0 +1,5 @@
+#!/bin/#!/usr/bin/env bash
+
+# to launch this playbook : start_other_master_node_in_k3s_cluster.yml
+
+ansible-playbook /opt/semaphore/Create-K3S-Cluster-Ansible-Playbook/playbooks/start_other_master_node_in_k3s_cluster.yml
---
+++ playbooks/start_worker_node_in_k3s_cluster.sh
@@ -0,0 +1,5 @@
+#!/bin/#!/usr/bin/env bash
+
+# to launch this playbook :start_worker_node_in_k3s_cluster.yml
+
+ansible-playbook /opt/semaphore/Create-K3S-Cluster-Ansible-Playbook/playbooks/start_worker_node_in_k3s_cluster.yml
